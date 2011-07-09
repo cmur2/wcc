75e018afc0f7c280ea7d84be48992cb9f0b18819
posativ <info@posativ.org>
2011-07-09 19:45:18 +0200

    plain text diff in email
    Using pickle as storage, plain text is nice though, but hard to write. Now, basically we have a dict, which keys are urls holding the latest hash and the corresponding compresed website. When diffing, we uncompress and show the difference and compress the new website and hash.
    
    Hint: it could consume some memory to store every diff locally in one file, but it's zlib'd, which give us only ~ 1/3 of the original file.

--- wcc.py
+++ wcc.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+#
+# web change check, wcc.py -- influenced by cmur2
+# Used to run as cron. Do `python wcc.py URL`, you can specify multiple
+# URLs in one call, and make multiple calls of wcc.py at different times, of course.
 
 import sys; reload(sys)
 sys.setdefaultencoding('utf-8') # we always need failsafe utf-8, in some way
 
 import os
 import smtplib
+import pickle
 
 from hashlib import sha1
 from urllib import urlopen
 from email.mime.text import MIMEText
+from zlib import compress, decompress
+from difflib import Differ
+
+# This is the main config. Tempfile, where everything is stored.
+# server is a smtp-server, you can access (e.g. gmail)
+# authenticate with login and pass
+# mailto is the recipient, when the site has changed, including a basic diff
 
 TMP = '/var/tmp/wcc.tmp'
-server = "smtp-server"
+server = "smtp server"; port = 587
 username = "login"
-passwd = "yourpass"
+passwd = "pass"
+mailto = "you@example.org"
 
 def sendmail(fr, to, title, msg):
+    """stolen from the internet. No idea, what it does internally. Works.
+    Basically http://docs.python.org/library/email-examples.html"""
     
     msg = MIMEText(msg)
     msg['Subject'] = title
     msg['From'] = fr
     msg['To'] = to
     
-    s = smtplib.SMTP(server, 587)
+    s = smtplib.SMTP(server, port)
     s.ehlo()
     s.starttls()
     s.ehlo()
@@ -35,19 +50,39 @@ def sendmail(fr, to, title, msg):
 def checkforupdate(url):
     """checks wether website has changed using sha1 hash stored
     in /var/tmp/wcc.tmp. Notification using sendmail()."""
+    
+    data = urlopen(url).read()  
+    h = sha1(data).hexdigest()
+    
+    try:
+        dict = pickle.load(open(TMP, 'r'))
+    except EOFError:
+        dict = {}
+    
+    if not url in dict:
         
-    h = sha1(urlopen(url).read()).hexdigest()
-    hashes = [tuple(hash.split('\t')) for hash in open(TMP)]
+        # first run
+        dict[url] = (h, compress(data))
+    
+    elif dict.get(url)[0] != h:
+        '''hash has changed, now diffing and updating dict'''
+        
+        diff = []
+        old = decompress(dict.get(url)[1])
+        data = data.split('\n')
+        for i, line in enumerate(old.split('\n')):
+            if line != data[i]: # diff has a multiple of quadratic runtime, only diff, when needed
+                d = Differ()
+                diff.append('\n'.join(d.compare([line, ], [data[i], ])))
+                
+        sendmail("wcc.py", mailto, "%s has changed" % url,
+                 "%s.\n\n-- %s" % ('\n'.join(diff), url))
+                
+        dict[url] = (h, compress('\n'.join(data)))
+        
     
     fp = open(TMP, 'w')
-    if not (url, h+'\n') in hashes:
-        sendmail("wcc.py", "agleicha@gmail.com", "%s has changed" % url,
-                 "Your monitored site hash changed.\n\n%s" % url)
-    fp.write('%s\t%s' % (url, h+'\n'))
-        
-    for t in hashes:
-        if t[0] != url:
-            fp.write('%s\t%s' % tuple(t))
+    pickle.dump(dict, fp)
     fp.close()
 
 if __name__ == '__main__':
