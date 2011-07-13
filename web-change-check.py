136c81eb159cbed55b0f096e36c8b24498d4b8b3
cmur2 <chrnicolai@gmail.com>
2011-07-13 11:23:12 +0200

    Refactored config parsing and function structures

--- web-change-check.py
+++ web-change-check.py
@@ -71,105 +71,104 @@ def stripHTML(orig):
     # replace named html entities like &amp;
     return re.sub("&\w+;", conv, new)
 
-def main():
+def parseConfig(file):
+    sites = []
     # read all lines
-    for line in open(CONF, 'r'):
+    for line in open(file, 'r'):
         line = line.strip()
         if not re.match(r'^[^#]', line):
             continue
         
         args = [arg for arg in line.split(';')]
+        sites.append(Site(args[0], args[1], args[2:]))
+
+    return sites
+
+def main(site):
         
-        site = Site(args[0], args[1], args[2:])
+    # persistent files
+    MD5_FILE = os.path.join(PER_DIR, site.id+'.md5')
+    SITE_FILE = os.path.join(PER_DIR, site.id+'.site')
         
-        if DEBUG:
-            print "site: %s" % site.url
-            print "  striphtml: %s" % site.striphtml
-            print "  id: %s" % site.id
-            print "  shorturl: %s" % site.shorturl
+    # retrieve site
+    new_data = urllib.urlopen(site.url).read()
+    # hash before converting charset
+    new_md5 = hashlib.md5(new_data).hexdigest()
         
-        # persistent files
-        MD5_FILE = os.path.join(PER_DIR, site.id+'.md5')
-        SITE_FILE = os.path.join(PER_DIR, site.id+'.site')
-        
-        # retrieve site
-        new_data = urllib.urlopen(site.url).read()
-        # hash before converting charset
-        new_md5 = hashlib.md5(new_data).hexdigest()
-        
-        # detect encoding of site - assume UTF-8 as default
-        enc = "utf-8"
-        for line in new_data.splitlines(True):
-            if re.search(r'<meta.*?content-type.*?>', line, re.IGNORECASE):
-                # found line with meta tag containing content-type information
-                # now filter out the charset information:
-                match = re.search(r'<meta.*charset=([a-zA-Z0-9-]*).*', line)
-                if match.group(1) != "": enc = match.group(1).lower()
+    # detect encoding of site - assume UTF-8 as default
+    enc = "utf-8"
+    for line in new_data.splitlines(True):
+        if re.search(r'<meta.*?content-type.*?>', line, re.IGNORECASE):
+            # found line with meta tag containing content-type information
+            # now filter out the charset information:
+            match = re.search(r'<meta.*charset=([a-zA-Z0-9-]*).*', line)
+            if match.group(1) != "":
+                enc = match.group(1).lower()
                 break
                 
-        if DEBUG: print "  encoding: %s" % enc
+    if DEBUG: print "  encoding: %s" % enc
+
+    # new_data is available in utf-8 (system default encoding)
+    new_data = new_data.decode(enc)
         
-        # new_data is available in utf-8 (system default encoding)
-        new_data = new_data.decode(enc)
+    if not os.path.exists(MD5_FILE):
+        outmd5 = open(MD5_FILE, 'w')
+        outmd5.write(new_md5+'\n')
+        outmd5.close()
+            
+        outdata = open(SITE_FILE, 'w')
+        outdata.write(new_data+'\n')
+        outdata.close()
+        return
+            
+    inmd5 = open(MD5_FILE, 'r')
+    old_md5 = inmd5.readline().strip()
+    inmd5.close()
         
-        if not os.path.exists(MD5_FILE):
-            outmd5 = open(MD5_FILE, 'w')
-            outmd5.write(new_md5+'\n')
-            outmd5.close()
-            
-            outdata = open(SITE_FILE, 'w')
-            outdata.write(new_data+'\n')
-            outdata.close()
-            continue
-            
-        inmd5 = open(MD5_FILE, 'r')
-        old_md5 = inmd5.readline().strip()
-        inmd5.close()
+    indata = open(SITE_FILE, 'r')
+    old_data = indata.readlines()
+    indata.close()
         
-        indata = open(SITE_FILE, 'r')
-        old_data = indata.readlines()
-        indata.close()
-        
-        if old_md5 != new_md5:
-            if DEBUG:
-                print "  Change detected:"
-                print "    old md5: %s, new md5: %s" % (old_md5, new_md5)
+    if old_md5 != new_md5:
+        if DEBUG:
+            print "  Change detected:"
+            print "    old md5: %s, new md5: %s" % (old_md5, new_md5)
             
-            # content of email
-            content = "Change at %s - diff follows:\n\n" % site.url
+        # content of email
+        content = "Change at %s - diff follows:\n\n" % site.url
             
-            diffGen = difflib.unified_diff(
-                old_data,
-                new_data.splitlines(True),
-                "OLD",
-                "NEW",
-                '(%s)' % time.strftime('%Y-%m-%d %H:%M:%S',
-                                time.gmtime(os.path.getmtime(MD5_FILE))),
-                '(%s)' % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
-                1)
+        diffGen = difflib.unified_diff(
+            old_data,
+            new_data.splitlines(True),
+            "OLD",
+            "NEW",
+            '(%s)' % time.strftime('%Y-%m-%d %H:%M:%S',
+                            time.gmtime(os.path.getmtime(MD5_FILE))),
+            '(%s)' % time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
+            1)
             
-            diff = ''.join([line for line in diffGen])
+        diff = ''.join([line for line in diffGen])
             
-            if site.striphtml:
-                diff = stripHTML(diff)
+        if site.striphtml:
+            diff = stripHTML(diff)
             
-            content += diff
+        content += diff
                         
-            for addr in site.emails:
-                if DEBUG: print "    addr: %s" % addr
-                sendMail(content, "[%s] %s changed" % (TAG, site.shorturl), addr)
+        for addr in site.emails:
+            if DEBUG: print "    addr: %s" % addr
+            sendMail(content, "[%s] %s changed" % (TAG, site.shorturl), addr)
                 
-            # syslog connection
-            #logger -t "$TAG" "Change at $site (tag $id) detected"
+        # syslog connection
+        #logger -t "$TAG" "Change at $site (tag $id) detected"
             
-            # do update
-            outmd5 = open(MD5_FILE, 'w')
-            outmd5.write(new_md5+'\n')
-            outmd5.close()
+        # do update
+        outmd5 = open(MD5_FILE, 'w')
+        outmd5.write(new_md5+'\n')
+        outmd5.close()
             
-            outdata = open(SITE_FILE, 'w')
-            outdata.write(new_data+'\n')
-            outdata.close()
+        outdata = open(SITE_FILE, 'w')
+        outdata.write(new_data+'\n')
+        outdata.close()
 
 if __name__ == '__main__':
     from optparse import OptionParser
@@ -180,4 +179,11 @@ if __name__ == '__main__':
     (options, args) = parser.parse_args()
     DEBUG = options.verbose
     
-    main()
+    for site in parseConfig(CONF):
+        if DEBUG:
+            print "site: %s" % site.url
+            print "  striphtml: %s" % site.striphtml
+            print "  id: %s" % site.id
+            print "  shorturl: %s" % site.shorturl
+        
+        main(site)
