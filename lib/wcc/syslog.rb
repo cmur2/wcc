4e795a8f91acb1ee1fbbcd704713565b6a0f3f50
cn <chrnicolai@gmail.com>
2011-11-24 20:56:04 +0100

    syslog notificator now uses ruby stdlib instead of system("logger ...") call
    This works only on *nix.

--- lib/wcc/syslog.rb
+++ lib/wcc/syslog.rb
@@ -1,11 +1,31 @@
 
 module WCC
 	class SyslogNotificator
-		def initialize
+
+		LEVELS = ['crit', 'emerg', 'alert', 'err', 'warning', 'notice', 'info', 'debug']
+
+		def initialize(opts)
+			if LEVELS.include?(opts)	
+				@prio = opts
+				@enable = true
+			else
+				@enable = false
+				raise ArgumentError, "The given priority '#{opts}' is not known, use one of: #{LEVELS.join(', ')}."
+			end
+			begin
+				# from ruby std lib
+				require 'syslog'
+			rescue LoadError
+				@enable = false
+				raise ArgumentError, "Won't log to syslog since your system does NOT support syslog!"
+			end
 		end
 		
+		# TODO: ERB template for syslog
 		def notify!(data)
-			system("logger -t '#{data.tag}' 'Change at #{data.site.uri.to_s} (tag #{data.site.id}) detected'")
+			Syslog.open(data.tag, Syslog::LOG_PID | Syslog::LOG_CONS) do |s|
+				s.send(@prio.to_sym, "Change at #{data.site.uri.to_s} (tag #{data.site.id}) detected")
+			end if @enable
 		end
 		
 		def self.parse_conf(conf); {} end
